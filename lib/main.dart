e6747c783ce553687678ae431bba2a2240905bf4
Pham Viet Toan <toanpham622002@gmail.com>
2025-08-06 15:40:56 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'screens/home.dart';
@@ -8,8 +10,39 @@ import 'screens/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  FirebaseOptions? firebaseOptions;
+  switch (defaultTargetPlatform) {
+    case TargetPlatform.android:
+      firebaseOptions = FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_ANDROID_API_KEY']!,
+        appId: dotenv.env['FIREBASE_ANDROID_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_ANDROID_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_ANDROID_PROJECT_ID']!,
+        storageBucket: dotenv.env['FIREBASE_ANDROID_STORAGE_BUCKET']!,
+      );
+      break;
+    case TargetPlatform.iOS:
+      firebaseOptions = FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_IOS_API_KEY']!,
+        appId: dotenv.env['FIREBASE_IOS_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_IOS_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_IOS_PROJECT_ID']!,
+        storageBucket: dotenv.env['FIREBASE_IOS_STORAGE_BUCKET']!,
+        iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID']!,
+      );
+      break;
+    default:
+      throw UnsupportedError('FirebaseOptions are not supported for this platform in .env.');
+  }
+
+  if (firebaseOptions != null) {
+    await Firebase.initializeApp(options: firebaseOptions);
+  }
+
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  await Firebase.initializeApp();
   bool googleLoggedIn = prefs.getBool('googleLoggedIn') ?? false;
   bool stayLoggedIn = prefs.getBool('stayLoggedIn') ?? false;
   User? user = FirebaseAuth.instance.currentUser;
